91a2deced286395eb2f550761c08cb7d897da545
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-05-11 03:43:40 +0300

    Update Footer.jsx

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -1,7 +1,35 @@
 import React from "react";
 
+// css
+import "./Footer.css";
+
+// assets
+import fb from "../../Assets/facebook.png";
+import twitter from "../../Assets/twitter.png";
+import LinkedIn from "../../Assets/linkedin.png";
+import inst from "../../Assets/instagram.png";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div className="sb-footer-section-padding">
+        <div className="sb-footer-links">
+          <div className="sb-footer-links-div">
+            <h4>For Business</h4>
+            <a href="/employer">
+              <p>Employer</p>
+            </a>
+            <a href="/healthplans">
+              <p>Health Plans</p>
+            </a>
+            <a href="/individuals">
+              <p>Individuals</p>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
